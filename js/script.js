429c9cdf3c7c60a56ce0ddc4f88dd8bb6fde243b
bahanm <bahanm@gmail.com>
2021-11-26 02:14:38 -0800

    worked on code for lightswitch toggle.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
+
+
+
+//set name of dog on start page
+let dogName = "";
+async function setName(e) {
+    e.preventDefault();
+    dogName = document.getElementById("inputName").value;
+    $('#nameStatus').html(`${dogName}'s Status`);
+    // console.log(dogName);
+    // console.log(`${dogName}'s Status`)
+    //Just to check it works
+    $('#nameIt').html(`You have named your new best friend ${dogName}`);
+}
+//turn lights on and off on main page
 //when click the light switch, add
 //box-shadow: inset 0 0 0 1000px rgba(0,0,0,.2);
 //to mainPage and statusWindow css
-
-function setName() {
-    const dogName = document.getElementById("inputName").value;
-    $('#status').html(`${dogName}'s Status`);
-    console.log(dogName);
-    console.log(`${dogName}'s Status`)
+let lightsOn=true;
+function turnLightsOn() {
+    $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
+    $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
+    lightsOn=true;
+}
+function turnLightsOff() {
+    $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
+    $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
+    lightsOn=false;
 }
 
+window.addEventListener('keydown',
+function(){lightsOn?turnLightsOff():turnLightsOn()} )
+
+
 $("form").on("submit", setName)
+
+$("light").on("click",)
