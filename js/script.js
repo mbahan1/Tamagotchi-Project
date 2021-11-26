172dfdcdc022722f74d4157aed37b37df6b597a0
bahanm <bahanm@gmail.com>
2021-11-26 01:17:35 -0800

    coded name entry form and event listening function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 //when click the light switch, add
 //box-shadow: inset 0 0 0 1000px rgba(0,0,0,.2);
 //to mainPage and statusWindow css
+
+function setName() {
+    const dogName = document.getElementById("inputName").value;
+    $('#status').html(`${dogName}'s Status`);
+    console.log(dogName);
+    console.log(`${dogName}'s Status`)
+}
+
+$("form").on("submit", setName)
