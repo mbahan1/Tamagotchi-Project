9a8a4344297530d209949a3e675f72b9f4546c12
bahanm <bahanm@gmail.com>
2021-11-27 17:12:34 -0800

    fixed instructions overlay, page blank but progression and buttons work.

--- js/script.js
+++ js/script.js
@@ -10,34 +10,44 @@ async function setName(e) {
     console.log(dogName);
     console.log(`${dogName}'s Status`)    
     $('#nameIt').html(`You have named your new best friend ${dogName}`);
+    $("#dogPic").attr("src","../images/sitting-eyes-yellow.png");
     await new Promise(resolve => setTimeout(resolve, 3000));
-    // wait 3 seconds after naming
+    // wait 3 seconds after naming    
     $("#startPageOverlay").css("display", "none");
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    $("#instructions").css("display", "normal");
-    //normal is empty placeholder to be toggled with "none"
+    $("#dogPic").attr("src","../images/sitting-cropped-yellow.png");
 }
+async function hideInstructions(e) {
+    e.preventDefault();
+    // await new Promise(resolve => setTimeout(resolve, 4000));
+    $("#instructions").css("display", "none");
+}
+
 
 //turn lights on and off on main page
 //when click the light switch, add
 //box-shadow: inset 0 0 0 1000px rgba(0,0,0,.2);
 //to mainPage and statusWindow css
-let lightsOn=true;
-function turnLightsOn() {
-    $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
-    $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
-    lightsOn=true;
-}
-function turnLightsOff() {
-    $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
-    $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
-    lightsOn=false;
-}
+// let lightsOn=true;
+// function turnLightsOn() {
+//     $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
+//     $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,0)");
+//     lightsOn=true;
+// }
+// function turnLightsOff() {
+//     $("#mainPage").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
+//     $("#statusWindow").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0,.4)");
+//     lightsOn=false;
+// }
 
 // window.addEventListener('keydown',
 // function(){lightsOn?turnLightsOff():turnLightsOn()} )
 
 
 $("form").on("submit", setName)
+document.getElementById("gotIt").addEventListener("click", function() {
+    $("#instructions").css("display", "none");
+});
+// $("form").on("submit", showInstructions)
+
 
 // $("light").on("click",)
